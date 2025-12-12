09ec49bf162cbf3cd3e5264f372d33f24437d567
vsaravind01 <vsaravind01@gmail.com>
2025-12-12 20:25:08 +0530

    Refactor: fix import statements in core module

--- deepchem_server/core/__init__.py
+++ deepchem_server/core/__init__.py
@@ -5,12 +5,12 @@ from deepchem_server.core.common.cards import Card, DataCard, ModelCard
 from deepchem_server.core.common.address import DeepchemAddress
 from deepchem_server.core.common.config import set_datastore, get_datastore
 from deepchem_server.core.common.progress_logger import log_progress
-from deepchem_server.core.common.model_mappings import model_address_map, featurizer_map
+from deepchem_server.core.common.model_mappings import model_address_map
 
 from deepchem_server.core.primitives import evaluator
 from deepchem_server.core.primitives import splitter
 from deepchem_server.core.primitives.evaluator import model_evaluator
-from deepchem_server.core.primitives.feat import featurize
+from deepchem_server.core.primitives.feat import featurize, featurizer_map
 from deepchem_server.core.primitives.inference import infer
 from deepchem_server.core.primitives.splitter import train_valid_test_split
 from deepchem_server.core.primitives.train import train
