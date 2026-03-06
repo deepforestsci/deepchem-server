27f3a2e5a9a366c17c39a36653b489297226edca
vsaravind01 <vsaravind01@gmail.com>
2026-03-06 23:13:57 +0530

    Fix: flake8 fix

--- pyds/tests/test_partition.py
+++ pyds/tests/test_partition.py
@@ -8,7 +8,7 @@ import uuid
 import pytest
 
 from pyds.data import Data
-from pyds.primitives import Featurize, Partition
+from pyds.primitives import Partition
 from pyds.settings import Settings
 
 
@@ -64,9 +64,7 @@ class TestPartition:
         )
         dataset_address = upload_result["dataset_address"]
 
-        result = live_partition_client.run(
-            dataset_address=dataset_address,
-        )
+        result = live_partition_client.run(dataset_address=dataset_address,)
 
         assert "partitioned_dataset_addresses" in result
 
