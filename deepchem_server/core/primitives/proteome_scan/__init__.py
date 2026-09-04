b58e04cabef00bc1ac001b1a5c8a7f96d209267d
vsaravind01 <vsaravind01@gmail.com>
2026-09-04 22:17:48 +0530

    yapf fixes

--- deepchem_server/core/primitives/proteome_scan/__init__.py
+++ deepchem_server/core/primitives/proteome_scan/__init__.py
@@ -1,10 +1,7 @@
-from deepchem_server.core.primitives.proteome_scan.pdb_clean import pdb_clean
 from deepchem_server.core.primitives.proteome_scan.ligand_prep import ligand_prep
-
-
-__all__ = []
+from deepchem_server.core.primitives.proteome_scan.pdb_clean import pdb_clean
 
 __all__ = [
-  "pdb_clean"
-  "ligand_prep",
+    "pdb_clean",
+    "ligand_prep",
 ]
