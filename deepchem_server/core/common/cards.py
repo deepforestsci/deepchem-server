2746b65e12cbb1b6b8572824828248b44a5de3eb
vsaravind01 <vsaravind01@gmail.com>
2026-09-18 19:18:32 +0530

    fix: yapf fix

--- deepchem_server/core/common/cards.py
+++ deepchem_server/core/common/cards.py
@@ -100,8 +100,8 @@ class DataCard(Card):
         "sdf",
     ]
     SUPPORTED_FILE_TYPES = [
-        'csv', 'dir', 'json', 'pdb', 'fasta', 'fastq', 'png', 'sdf', 'dcd', 'txt', 'xml', 'py', 'ds', 'dcs', 'md', 'pdbqt', 'zip', 'smi',
-        'smiles', 'bz2', 'cxsmiles', 'onnx', 'hdf5', 'log'
+        'csv', 'dir', 'json', 'pdb', 'fasta', 'fastq', 'png', 'sdf', 'dcd', 'txt', 'xml', 'py', 'ds', 'dcs', 'md',
+        'pdbqt', 'zip', 'smi', 'smiles', 'bz2', 'cxsmiles', 'onnx', 'hdf5', 'log'
     ]
 
     def __init__(self,
